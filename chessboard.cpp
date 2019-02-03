7317e258f07431974b45d312375f9285983e5e67
John Hicks <hicks.websites@gmail.com>
2019-02-03 15:00:50 -0500

    Ignore binary on linux

--- chessboard.cpp
+++ chessboard.cpp
@@ -10,15 +10,24 @@
 using namespace std;
 
 void PrintRow(string& row, int times);
+int PromptForSideLength();
 
 
 const string BLACK = "********";  // Define a line of a black square
 const string WHITE = "        ";  // Define a line of a white square
+const int MAX_SQUARE_SIZE = 100;  // Maximum number of characters allowed for each square's side length
 
 int main ()
 {
   string whiteRow;            // A row beginning with a white square
   string blackRow;            // A row beginning with a black square
+  int squareSideLength = PromptForSideLength();   // The length to use for each of the chess board's squares
+
+
+
+
+
+
 
   // Create a white-black row by concatenating the basic strings
   whiteRow = WHITE + BLACK + WHITE + BLACK +
@@ -50,5 +59,20 @@ void PrintRow(string& row, int times) {
     for (int i = 0; i < 5; i++) {
         cout << row << endl;
     }
+}
+
+int PromptForSideLength() {
+    int len = 0;
+    cout << "Please enter a side length to use for the chess board's squares, then press enter:";
+    cin >> len;
+    while(!cin || (len < 1 || len > MAX_SQUARE_SIZE)) {
+        if (!cin) {
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+        cout << "\nThat entry is invalid. Please try again by entering a number between 1 and " << MAX_SQUARE_SIZE << ": ";
+        cin >> len;
+    }
+    return len;
 }
 
